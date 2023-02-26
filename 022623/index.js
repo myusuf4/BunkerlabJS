10c545a0964716a50394cdb5f5ab7792c0d88c93
Myusuf04 <nurmurzayevmuhammad@gmail.com>
2023-02-26 19:44:57 +0500

    bunkerlabJS Comparision

---
+++ 022623/index.js
@@ -0,0 +1,78 @@
+// let num=24.3;
+
+// console.log(Number(num));
+// console.log(Number.parseFloat(num));
+// console.log(Number.parseInt(num));
+
+// let str=1;
+// let str2="1";
+// console.log(str1 + str2);
+// console.log(str1 + +str2);
+
+// == === > < != => =<
+
+// let num=22;
+// let num2=22;
+
+// console.log(num===num2);
+// console.log(num==num2);
+// console.log(num < num2);
+// console.log(num != num2);
+
+
+// let num = 2;
+// num++;
+
+// console.log(num++)
+// console.log(num)
+
+// || &&
+
+// console.log(true || false);
+// console.log(true && true);
+
+// home
+// let a=1; b=1;
+// let c=++a;
+// // 2 
+// let d=b++;
+// // 1 chunki birinchi tenglaydi;
+
+// let a=2;
+// let x=1+(a*=2)
+
+// // a 4 boladi
+// // x 5 boladi
+
+// console.log("" + 1 + 0); // 10 String
+
+// console.log("" - 1 + 0); // -1 Number
+
+// console.log(true + false); // 1 Number
+
+// console.log(6/"3"); //2 Number
+
+// console.log("2" * "3");  // 6 NUmber
+
+// console.log(4 + 5 + "px");// 9px Str
+
+// console.log("$" + 4 + 5)//$45
+
+// console.log("4" -2); // 2 NUm
+
+// console.log("4px" - 2);//NaN
+
+// console.log(" -9 " + 5);// -9 5
+// console.log(" -9 " - 5);// -14
+ 
+// console.log(null + 1);//1
+
+// console.log(undefined+1);//Nan
+
+// console.log("/t /n" +1);/t /n1
+
+// 5>4 //true
+// apple > pineapple false;
+//undefined==null true
+// undefined === null false
+
