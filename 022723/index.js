88f995cfa66e272114441152450e4478459deae2
Myusuf04 <nurmurzayevmuhammad@gmail.com>
2023-02-27 11:59:06 +0500

    if else,switch, foor loop,nulish

---
+++ 022723/index.js
@@ -0,0 +1,75 @@
+  // if
+
+//   if(shart){vazifa}
+
+// let ball = 70;
+
+// let user1=50; // Rejected
+// let user2=70; // Superkontrakt
+// let user3=90; // Accepted
+
+// if(user2 > 70){
+    
+//     console.log("Accepted");
+// }
+// else if(user2 == 70){
+    
+//     console.log("Superkontrakt");
+// }
+// else{
+    
+//     console.log("Rejected");
+// }
+
+
+// Ternary Operators ?:
+
+// let ball = 70;
+
+// let user1=50; // Rejected
+// let user2=70; // Superkontrakt
+// let user3=90; // Accepted
+
+// user1 > ball ? console.log("Accepted") : user1 == ball ? console.log("Superkontrakt"):console.log("Rejected");
+
+
+
+// Switch case // switchda doim aniq ma'lumotlar tekshiriladi
+// Switch(o'zgruvchi){case  xolati : task}
+
+// let month=7;
+
+// switch(month){
+//     case 1: console.log("Yanvar"); 
+//     break;
+//     case 2: console.log("Fevral"); //no'tugri bolsa
+//     break;
+//     case 3: console.log("Salom");
+//     break;
+//     default:console.log("not found");// else
+// }
+
+
+// Nulish ?? false=>null, undefined
+
+// console.log(null|| "teacher");// teacher
+// console.log(null && "teacher");//null
+// console.log(null || "teacher");// teacher
+
+
+// console.log(false ?? null);
+
+
+// for loop for(qayerdamiz, qayerga boramiz, nechta qadamdan boramiz){vazifa}
+
+// for(){}
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// for(let i = 1; i<=10; i++){
+//     console.log(i);
+// }
