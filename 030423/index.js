a4980360bf32d6a6ec9557753953a6874658b89a
Myusuf04 <nurmurzayevmuhammad@gmail.com>
2023-03-04 20:27:07 +0500

    Object

---
+++ 030423/index.js
@@ -0,0 +1,44 @@
+// let obj={};
+// let obj1=new Object();
+// console.log(obj);
+// console.log(obj1);
+
+
+// ==========
+// let user={
+//     name:"Muhammad",
+//     age:16,
+// }
+// console.log(user);
+
+
+// ==========
+
+// let user={
+//     name:"Muhammad",
+//     age:16,
+// }
+// console.log(user.name);
+// console.log(user.age);
+
+
+// ==========
+
+// let user={
+//     name:"Muhammad",
+//     age:16,
+// }
+
+// console.log(user.surname)
+
+// ==========
+
+// let user={
+//     name:"Muhammad",
+//     age:16,
+// }
+
+// user.surname="Nurmirzayev";
+
+// console.log(user);
+
