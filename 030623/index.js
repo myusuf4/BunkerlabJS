53c6df0622e3d701902e73060fc055cbf3b0cfbf
Myusuf04 <nurmurzayevmuhammad@gmail.com>
2023-03-07 20:01:31 +0500

    object this

---
+++ 030623/index.js
@@ -0,0 +1,37 @@
+// object
+
+// let obj={name:"Muhammad"};
+// console.log(obj);
+
+// let obj={};
+// let ob=new Object();
+
+// console.log(obj);
+// console.log(ob);
+
+// let user={
+//     name:"Muhammad",
+//     surname:"Nurmirzayev",
+//     "full name":"Nurmirzayev Muhammad"
+// }
+
+// let key="name";
+
+// console.log(user[key]);// === user.name
+// console.log(user["full name"]);
+// console.log(user.name);
+// console.log(user.surname);
+
+
+let user={
+        name:"Muhammad",
+        surname:"Nurmirzayev",
+        "full name":"Nurmirzayev Muhammad"
+    }
+
+// user.name="Muhiddin"  
+delete user.surname;  
+console.log(user);
+
+
+
---
+++ 030723/index.js
@@ -0,0 +1,20 @@
+// let user = {
+//     name: "John",
+//     age: 30
+//   };
+  
+//   user.sayHi = function() {
+//     console.log("Hello!");
+//   };
+  
+//   user.sayHi(); // Hello!
+
+// let user = {
+//     name: "John",
+//     age: 30,
+  
+//     sayHi() {
+//       console.log(user.name); // "user" instead of "this"
+//     }
+  
+//   };
