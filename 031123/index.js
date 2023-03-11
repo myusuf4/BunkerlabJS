505a55d31835d195d7224aba5604023ba56d3e03
Myusuf04 <nurmurzayevmuhammad@gmail.com>
2023-03-11 20:03:04 +0500

    Num

---
+++ 031123/index.js
@@ -0,0 +1,8 @@
+// console.log(isNaN(NaN));
+// console.log(isNaN("str"));
+
+// console.log(NaN === NaN);
+
+// console.log( isFinite("15") ); 
+// console.log( isFinite("str") ); 
+// console.log( isFinite(Infinity) ); 
