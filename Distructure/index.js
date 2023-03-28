56126d77fea441f0d35c9cd441e59437660b19bf
myusuf4 <nurmurzayevmuhammad@gmail.com>
2023-03-28 12:42:06 +0500

    changing keys name

--- Distructure/index.js
+++ Distructure/index.js
@@ -1,7 +1,16 @@
-let obj={name:"Muhammad","hi":"Js",age:18};
+// let obj={name:"Muhammad","hi":"Js",age:18};
 
-let {name,hi,age}=obj;
+// let {name,hi,age}=obj;
 
-console.log(name);
-console.log(hi);
-console.log(age);
+// console.log(name);
+// console.log(hi);
+// console.log(age);
+
+
+// let name="Muhammadd";
+// let {name:newName,hi,age}=obj;
+
+// console.log(name);
+// console.log(newName);
+
+
