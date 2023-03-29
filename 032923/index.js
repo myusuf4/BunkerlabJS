6642ee5d68cad8632d585ceaa7fd8e4d6b4042a3
myusuf4 <nurmurzayevmuhammad@gmail.com>
2023-03-29 11:42:57 +0500

    Distructure

---
+++ 032923/index.js
@@ -0,0 +1,39 @@
+// let obj={
+//     name:"Muhammad",
+//     age:16,
+//     focusedOn:"Frontend"
+// }
+// let name="Abdulloh";
+// let {name:newName,age,focusedOn}=obj;
+
+// console.log(name);
+// console.log(newName);
+// console.log(age);
+// console.log(focusedOn);
+
+
+// let obj={
+//     name:"Muhammad",
+//     age:16,
+//     focusedOn:"Frontend",
+//     child:{
+//         name:"Abdulloh"
+//     }
+// };
+
+// let {name,age,focusedOn,child:{name:newName}}=obj
+// console.log(name);
+// console.log(age);
+// console.log(focusedOn);
+// console.log(newName);
+
+
+let arr=["apple","kiwi","banana"];
+
+let [bir,...rest]=arr;
+let [ikki,uch]=rest
+
+console.log(bir);
+console.log(rest);
+console.log(ikki);
+console.log(uch);
