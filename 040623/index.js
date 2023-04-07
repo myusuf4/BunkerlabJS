948b87fdd31e4df53b11e8723512d201d9eee8d4
myusuf4 <nurmurzayevmuhammad@gmail.com>
2023-04-07 10:57:03 +0500

    getter-setter

---
+++ 040623/index.js
@@ -0,0 +1,18 @@
+let user = {
+    get name() {
+      return this._name;
+    },
+  
+    set name(value) {
+      if (value.length < 4) {
+        console.log("Name is too short, need at least 4 characters");
+        return;
+      }
+      this._name = value;
+    }
+  };
+  
+  user.name = "Pete";
+  console.log(user.name); // Pete
+  
+  user.name = ""; // Name is too short...
