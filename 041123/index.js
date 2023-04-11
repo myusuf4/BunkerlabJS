3eef27d02903703d6434ef876ebf2d990340539e
myusuf4 <nurmurzayevmuhammad@gmail.com>
2023-04-11 14:50:37 +0500

    s

---
+++ 041123/index.js
@@ -0,0 +1,65 @@
+// let d={
+//     user:{name:"Muhammad",password:1234}
+// }
+// console.log("start")
+// function login(n,p,call) {
+//     setTimeout(()=>{
+//         if(d.user.name===n && d.user.password===p){
+//             return call("welcome");
+//         }
+//         else{
+//             return call ("smth wrong")
+//         }
+        
+//     },1000)
+// }
+
+// login("Muhammad",1234,(usr)=>{
+// console.log(usr);
+// });
+// console.log("finished");
+
+// let d={
+//     user:{name:"Muhammad",password:1234}
+// }
+// console.log("start");
+// let user1=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+
+//         if(d.user.name=="Muhammad"&& d.user.password==1234){
+//             resolve("welcome")
+//         }
+//         else{ reject("smth wrrong")}
+//     },1000)
+//     })
+// user1.then((r)=>console.log(r))
+// user1.catch((e)=>console.log(e))
+// user1.finally(()=>console.log("finished"))
+
+
+
+
+
+// let d={
+//     user:{name:"Muhammad",password:1234}
+// }
+// console.log("start");
+// let user1=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+
+//         if(d.user.name=="Muhammad"&& d.user.password==1234){
+//             resolve("welcome")
+//         }
+//         else{ reject("smth wrrong")}
+//     },1000)
+//     })
+
+
+// async function get(){
+//     let res=await user1;
+//     console.log(res);
+//     console.log("end");
+// }
+// get()
+// console.log(bir.outerHTML="<h2>salom</h2>");
+// console.log(bir.textContent="salom");
